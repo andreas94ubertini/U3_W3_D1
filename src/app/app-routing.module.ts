9c2e7c4b371ca2817ebca0e7e89e1df85d4d953a
andreas ubertini <ubertiniandreas@gmail.com>
2023-07-31 16:03:28 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {FavouriteComponent} from "./pages/favourite/favourite.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'preferiti',
+    component:FavouriteComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FavouriteComponent } from './pages/favourite/favourite.component';
+import { NavComponent } from './component/nav/nav.component';
+import { ProductComponent } from './component/product/product.component';
+import {HttpClientModule} from "@angular/common/http";
+import { FavouriterowComponent } from './component/favouriterow/favouriterow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavouriteComponent,
+    NavComponent,
+    ProductComponent,
+    FavouriterowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
